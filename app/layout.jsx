d51a61567d978aa82c0ebd9a7ac107598ae3d68d
busratok <busratokuc@gmail.com>
2024-05-26 21:23:15 +0100

    up

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,9 +14,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={roboto.className}>
-        <Header />
+        {/* <Header /> */}
         {children}
-        <Footer />
+        {/* <Footer /> */}
       </body>
     </html>
   );

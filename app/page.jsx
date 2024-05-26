9527b34964b15312b89f5edcd017825235f261ad
busratok <busratokuc@gmail.com>
2024-05-27 00:47:04 +0100

    up

--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,5 @@
 import Hero from "./_components/Hero";
 import Categories from "./_components/Categories";
-import HospitalList from "./_components/HospitalList";
-import OurServices from "./_components/OurServices";
-import Gallery from "./_components/Gallery";
-import Testimonials from "./_components/Testimonials";
-import BeforeAfterComponent from "./_components/BeforeAfterComponent";
 
 export default function Home() {
   return (

625a6d078d78487ea93099eb7d58f42c82b5873b
busratok <busratokuc@gmail.com>
2024-05-27 23:45:52 +0100

    up

--- app/api/revalidate/route.js
+++ app/api/revalidate/route.js
@@ -1,8 +1,7 @@
 import { revalidateTag } from "next/cache";
 
 export async function POST(req) {
-  const tag = req.body?.model;
-  console.log(req.body);
+  const tag = await req.body?.model;
   const url = new URL(req.url, `http://${req.headers.host}`);
   const secret = url.searchParams.get("secret");
 
